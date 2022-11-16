1f12bca08cdc072c0af41993146f8a355809f04b
Felipe Sousa <felipe.fps09@hotmail.com>
2022-11-16 09:16:37 -0300

    Create routes REST, animes, movies, users and genres

---
+++ backend/src/main/java/com/animeson/projectanime/resources/GenreResource.java
@@ -0,0 +1,10 @@
+package com.animeson.projectanime.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/genres")
+public class GenreResource {
+
+}
