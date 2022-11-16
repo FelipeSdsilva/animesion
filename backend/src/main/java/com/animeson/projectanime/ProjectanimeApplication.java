9a7d47a99df1bd317864053039d1610726bfa879
Felipe Sousa <felipe.fps09@hotmail.com>
2022-11-15 21:15:05 -0300

    Initializing project

---
+++ backend/src/main/java/com/animeson/projectanime/ProjectanimeApplication.java
@@ -0,0 +1,13 @@
+package com.animeson.projectanime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectanimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectanimeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/animeson/projectanime/ProjectanimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animeson.projectanime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectanimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
