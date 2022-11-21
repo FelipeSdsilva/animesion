fca24a12810f0b381895bfc4a4e7a027441bd5d0
Felipe Sousa <felipe.fps09@hotmail.com>
2022-11-21 16:58:15 -0300

    Find user by id

--- backend/src/main/java/com/animeson/projectanime/resources/UserResource.java
+++ backend/src/main/java/com/animeson/projectanime/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +24,11 @@ public class UserResource {
 		List<UserDTO> listUser = userService.findAllUser();
 		return ResponseEntity.ok().body(listUser);
 	}
+	
+	@GetMapping(value = "{id}")
+	public ResponseEntity<UserDTO> findUserById(@PathVariable Long id){
+		UserDTO userDto = userService.findUserById(id);
+		return ResponseEntity.ok().body(userDto);
+	}
+	
 }
