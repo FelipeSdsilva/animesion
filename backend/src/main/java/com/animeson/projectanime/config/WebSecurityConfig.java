2c397b83b67d750898004e959118cc0113328b7d
Felipe Sousa <felipe.fps09@hotmail.com>
2022-11-21 18:57:21 -0300

    Open cors test

--- backend/src/main/java/com/animeson/projectanime/config/WebSecurityConfig.java
+++ backend/src/main/java/com/animeson/projectanime/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.animeson.projectanime.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -30,4 +35,12 @@ public class WebSecurityConfig {
 		return http.build();
 	}
 
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 }
--- backend/src/main/java/com/animeson/projectanime/resources/UserResource.java
+++ backend/src/main/java/com/animeson/projectanime/resources/UserResource.java
@@ -1,15 +1,20 @@
 package com.animeson.projectanime.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.animeson.projectanime.dto.UserDTO;
+import com.animeson.projectanime.dto.UserInsertDTO;
 import com.animeson.projectanime.services.UserService;
 
 @RestController
@@ -31,4 +36,11 @@ public class UserResource {
 		return ResponseEntity.ok().body(userDto);
 	}
 	
+	@PostMapping
+	public ResponseEntity<UserDTO> insertNewUser(@RequestBody UserInsertDTO userInDto){
+		UserDTO userDto = userService.insertNewUser(userInDto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(userDto.getId()).toUri();
+		return ResponseEntity.created(uri).body(userDto);
+	}
 }
